0f17006f07c6f832c3763be372155adbca3b1108
cerisier823 <cerisier823@gmail.com>
2024-10-03 23:51:07 +0900

    kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎</title>
+</head>
+<body>
+    <?php 
+        $item = ['name' => 'onion', 'price' => 200, 'weight' => 160 ];
+        print_r($item);
+    ?>
+</body>
+</html>
